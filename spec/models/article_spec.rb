7314544ce3f43605d236e20432d965dc59747d4b
bertrand <mutanganabertrand@gmail.com>
2022-12-09 13:55:05 +0200

    Add tests configuration and wrote the first model test

---
+++ spec/models/article_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe Article, type: :model do
+  before :each do
+    @article = create(:article)
+  end
+
+  it 'is valid with valid attributes' do
+    expect(@article).to be_valid
+  end
+end
