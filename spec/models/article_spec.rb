b24e9799bb965c78464537b46ba911cd54ea6295
bertrand <mutanganabertrand@gmail.com>
2022-12-09 13:59:52 +0200

    Write tests for the article model

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -8,4 +8,34 @@ RSpec.describe Article, type: :model do
   it 'is valid with valid attributes' do
     expect(@article).to be_valid
   end
+
+  it 'is not valid without a title' do
+    @article.title = nil
+    expect(@article).to_not be_valid
+  end
+
+  it 'is not valid without a body' do
+    @article.body = nil
+    expect(@article).to_not be_valid
+  end
+
+  it ' is not valid if title is too short' do
+    @article.title = 'a' * 6
+    expect(@article).to_not be_valid
+  end
+
+  it 'is not valid if title is too long' do
+    @article.title = 'a' * 61
+    expect(@article).to_not be_valid
+  end
+
+  it 'is not valid if body is too short' do
+    @article.body = 'a' * 17
+    expect(@article).to_not be_valid
+  end
+
+  it 'is not valid if body is too long' do
+    @article.body = 'a' * 301
+    expect(@article).to_not be_valid
+  end
 end
