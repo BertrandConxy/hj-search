e41710961b028d9ed7d34e3f166b9c805ebec84f
bertrand <mutanganabertrand@gmail.com>
2022-12-09 10:43:05 +0200

    Add controllers and Views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'analytics/index'
+  get 'articles/index'
+  get 'articles/show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "articles#index"
 end
