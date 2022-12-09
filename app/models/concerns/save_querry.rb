b110435eca10496578b929d77ca1215eecc4017e
bertrand <mutanganabertrand@gmail.com>
2022-12-09 12:37:25 +0200

    Modify save querry

--- app/models/concerns/save_querry.rb
+++ app/models/concerns/save_querry.rb
@@ -2,13 +2,13 @@ module SaveQuerry
     def self.save(query, session, articles)
       return if query.nil? || query.length < 3
   
-      recent_search = Analytic.new(query: query, articles: articles)
-      recent_search.session_id = session
-      session_search = Analytic.where(session_id: session).last
-      if session_search.nil? || !session_search.searched?(query)
-        recent_search.save
-      elsif session_search.query.length < query.length
-        session_search.update(query: query, articles: articles)
+      recent_querry = Analytic.new(query: query, articles: articles)
+      recent_querry.session_id = session
+      prev_querry = Analytic.where(session_id: session).last
+      if prev_querry.nil? || !prev_querry.searched?(query)
+        recent_querry.save
+      elsif prev_querry.query.length < query.length
+        prev_querry.update(query: query, articles: articles)
       end
     end
   end
