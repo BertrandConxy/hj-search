64ac4fea89c6afb660b8649899c8f21c13da60d5
bertrand <mutanganabertrand@gmail.com>
2023-01-25 13:02:54 +0200

    Add deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 require "active_support/core_ext/integer/time"
 
-Rails.application.routes.default_url_options[:host] = "hj-search-production.up.railway.app"
+# Rails.application.routes.default_url_options[:host] = "hj-search-production.up.railway.app"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   # Code is not reloaded between requests.
